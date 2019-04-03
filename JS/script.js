8663d5c016582da41ce1e49609ac581ff08e70e3
asamatokhin <asamatokhin@gmail.com>
2019-04-03 23:29:09 +0400

    подключил карусельный слайдер

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,35 @@
 $(document).ready(function () {
-    window.onload = function () {
-        function clock() {
-            var date = new Date(),
-                day = date.getDate(),
-                month = date.getMonth(),
-                monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                year = date.getFullYear(),
-                hour = date.getHours(),
-                min = date.getMinutes();
-            if (day < 10) day = "0" + day;
-            if (hour < 10) hour = "0" + hour;
-            if (min < 10) min = "0" + min;
-            document.getElementById("date").innerHTML = day + " " + monthArr[month] + " " + year;
-            document.getElementById("time").innerHTML = hour + ":" + min;
-        }
-
-        var timer;
-
-        function clockStart() {
-            timer = setInterval(clock, 1000);
-            clock();
-        }
-
-        clockStart();
-    }
+    // function clock() {
+    //     var date = new Date(),
+    //         day = date.getDate(),
+    //         month = date.getMonth(),
+    //         monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    //         year = date.getFullYear(),
+    //         hour = date.getHours(),
+    //         min = date.getMinutes();
+    //     if (day < 10) day = "0" + day;
+    //     if (hour < 10) hour = "0" + hour;
+    //     if (min < 10) min = "0" + min;
+    //     document.getElementById("date").innerHTML = day + " " + monthArr[month] + " " + year;
+    //     document.getElementById("time").innerHTML = hour + ":" + min;
+    // }
+    //
+    // var timer;
+    //
+    // function clockStart() {
+    //     timer = setInterval(clock, 1000);
+    //     clock();
+    // }
+    //
+    // clockStart();
+    var slider = $('.slides');
+    slider.owlCarousel({
+        items:4,
+        margin: 20,
+        dots: false,
+        nav: true,
+        navText: ["<",">"],
+        loop: true,
+        autoplay: false
+    });
 })
